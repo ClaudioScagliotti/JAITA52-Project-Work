0699cf0e64d8bf10d0ca41eb44651d34e83831dc
nzrv <96348033+nzr-vi@users.noreply.github.com>
2022-03-24 00:00:32 +0100

    deleting the files to re-upload so it rename the folder
    Packages should never start with Capitals!!!
    Git apparently is case insensitive so gonna remove the files and reupload so it fix the folder name.

--- ProjectWork/src/main/java/com/group/projectwork/repository/AlimentazioneRepo.java
+++
@@ -1,9 +0,0 @@
-package com.group.projectwork.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.group.projectwork.Entity.Alimentazione;
-
-public interface AlimentazioneRepo extends JpaRepository<Alimentazione, Integer> {
-
-}
--- ProjectWork/src/main/java/com/group/projectwork/repository/CategoriaRepo.java
+++
@@ -1,9 +0,0 @@
-package com.group.projectwork.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.group.projectwork.Entity.Categoria;
-
-public interface CategoriaRepo extends JpaRepository<Categoria, Integer> {
-
-}
