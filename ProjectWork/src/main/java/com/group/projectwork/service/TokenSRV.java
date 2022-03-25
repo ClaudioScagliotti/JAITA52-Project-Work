b1507b093dd41455ffb8ce3c2af2d4ff8a8b90b4
nzrv <96348033+nzr-vi@users.noreply.github.com>
2022-03-25 23:50:08 +0100

    Token generation duplicate fix
    Now on token generation the srv will look up for one already in DB before creating a new one

--- ProjectWork/src/main/java/com/group/projectwork/service/TokenSRV.java
+++ ProjectWork/src/main/java/com/group/projectwork/service/TokenSRV.java
@@ -52,8 +52,16 @@ public class TokenSRV {
     }
 
     public Token generate(Utente u) {
-    	Token token = new Token();
-    	token.setUtente(u);
+    	Token token;
+    	var prevToken = this.db.findByUtente(u);
+    	if(prevToken.isEmpty())
+    	{
+    		token = new Token();
+    		token.setUtente(u);
+    	}
+    	else
+    		token = prevToken.get();
+    	
     	token.setValore(StringUtils.random(25));
     	return this.updToken(token);
     }
