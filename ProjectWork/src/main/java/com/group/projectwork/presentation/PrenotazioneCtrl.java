e697d74bab0b29056affdb6e9be7fd73064ffe5c
nzrv <96348033+nzr-vi@users.noreply.github.com>
2022-03-28 16:20:40 +0200

    Get prenotazioni
    with list for active and complete

--- ProjectWork/src/main/java/com/group/projectwork/presentation/PrenotazioneCtrl.java
+++ ProjectWork/src/main/java/com/group/projectwork/presentation/PrenotazioneCtrl.java
@@ -1,5 +1,8 @@
 package com.group.projectwork.presentation;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,11 +10,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import com.group.projectwork.entity.Prenotazione.State;
+import com.group.projectwork.entity.Prenotazione;
 import com.group.projectwork.entity.Utente;
 import com.group.projectwork.service.PrenotazioneSRV;
 
 @Controller
-@RequestMapping("/lista-prenotazioni")
+@RequestMapping("/prenotazione")
 @SessionAttributes("utente")
 public class PrenotazioneCtrl {
 	
@@ -21,11 +26,21 @@ public class PrenotazioneCtrl {
 	@GetMapping
 	public String get(Utente utente, Model model){
 		if (utente.getEmail()==null) {
+			model.addAttribute("redirectTo","/prenotazione");
 			return "redirect:/login-page";
 		}
-		model.addAttribute("prenotazioni", srv.getListaPrenotazioni());
-
+		
+		var total = srv.getByUtente(utente);
+		List<Prenotazione> attive = new LinkedList<>();
+		List<Prenotazione> concluse = new LinkedList<>();
+		total.stream().forEach(p->{
+			if(p.getStato()==State.Corrente || p.getStato()==State.Prenotato)
+				attive.add(p);
+			else
+				concluse.add(p);
+		});
+		model.addAttribute("attive", attive);
+		model.addAttribute("concluse", concluse);
 		return "prenotazioni";
 	}
-	
 }
--- ProjectWork/src/main/java/com/group/projectwork/service/PrenotazioneSRV.java
+++ ProjectWork/src/main/java/com/group/projectwork/service/PrenotazioneSRV.java
@@ -10,52 +10,49 @@ import com.group.projectwork.entity.Prenotazione;
 import com.group.projectwork.entity.Prenotazione.State;
 import com.group.projectwork.entity.Utente;
 import com.group.projectwork.repository.PrenotazioneDB;
+
 @Service
 public class PrenotazioneSRV {
 
 	@Autowired
 	PrenotazioneDB pdb;
-	
-	List<Prenotazione> getAll(){
+
+	List<Prenotazione> getAll() {
 		return this.pdb.findAll();
 	}
-		
-    public Prenotazione addPrenotazione(Prenotazione p){
-    	return this.pdb.save(p);
-    }
-    
-    public void delPrenotazioneById(int id){
-    	this.pdb.deleteById(id);
-    }
-    
-    
-    public Prenotazione getById(int id) {
-   	 var opt = pdb.findById(id);
-   	 if(opt.isPresent())
-   		 return opt.get();
-   	 return null;
-     }
 
-	 public List<Prenotazione> getListaPrenotazioni() {
-		return this.pdb.findAll(); 
+	public Prenotazione addPrenotazione(Prenotazione p) {
+		return this.pdb.save(p);
 	}
-    
-    List<Prenotazione> getByStato(State s){
-    	return this.pdb.findAllByStato(s);
-    }
-    
-    List<Prenotazione> getByInizio(Date d){
-    	return this.pdb.findAllByInizio(d);
-    }
-    
-    List<Prenotazione> getByFine(Date d){
-    	return this.pdb.findAllByFine(d);
-    }
-    
-    List<Prenotazione> getByUtente(Utente u){
-    	return this.pdb.findAllByUtente(u);
-    }
-    
-    
-	
+
+	public void delPrenotazioneById(int id) {
+		this.pdb.deleteById(id);
+	}
+
+	public Prenotazione getById(int id) {
+		var opt = pdb.findById(id);
+		if (opt.isPresent())
+			return opt.get();
+		return null;
+	}
+
+	public List<Prenotazione> getListaPrenotazioni() {
+		return this.pdb.findAll();
+	}
+
+	public List<Prenotazione> getByStato(State s) {
+		return this.pdb.findAllByStato(s);
+	}
+
+	public List<Prenotazione> getByInizio(Date d) {
+		return this.pdb.findAllByInizio(d);
+	}
+
+	public List<Prenotazione> getByFine(Date d) {
+		return this.pdb.findAllByFine(d);
+	}
+
+	public List<Prenotazione> getByUtente(Utente u) {
+		return this.pdb.findAllByUtente(u);
+	}
 }
