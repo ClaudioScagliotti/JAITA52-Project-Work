9d17fca61a9727241fab178e5dd51d0add2dac76
ClaudioScagliotti <cscagliotti99@gmail.com>
2022-03-28 17:24:13 +0200

    Update PrenotazioneCtrl.java

--- ProjectWork/src/main/java/com/group/projectwork/presentation/PrenotazioneCtrl.java
+++ ProjectWork/src/main/java/com/group/projectwork/presentation/PrenotazioneCtrl.java
@@ -1,6 +1,6 @@
 package com.group.projectwork.presentation;
 
-import static com.group.projectwork.utility.ErrorUtils.accessDeniedMVC;
+import static com.group.projectwork.utility.ErrorUtils.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,12 +10,16 @@ import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.group.projectwork.dto.CreateVeicoloDTO;
+import com.group.projectwork.dto.PrenotazioneDTO;
 import com.group.projectwork.entity.Prenotazione;
+import com.group.projectwork.entity.Prenotazione.State;
 import com.group.projectwork.entity.Utente;
 import com.group.projectwork.entity.Utente.Role;
+import com.group.projectwork.entity.Veicolo;
 import com.group.projectwork.exception.ImageSaveException;
 import com.group.projectwork.exception.VeicoloParseException;
 import com.group.projectwork.service.PrenotazioneSRV;
+import com.group.projectwork.service.VeicoloSRV;
 
 @Controller
 @SessionAttributes("utente")
@@ -23,22 +27,32 @@ public class PrenotazioneCtrl {
 	
 	@Autowired
 	PrenotazioneSRV srv;
+	
+	@Autowired
+	VeicoloSRV vSrv;
 
 	@PostMapping("/add")
-	public String addVeicolo( Utente loggedIn, Model model, Prenotazione p) {
+	public String addVeicolo( Utente loggedIn, Model model, PrenotazioneDTO p) {
 
-		if (!loggedIn.getRuolo().equals(Role.RUOLO_UTENTE) || !p.getVeicolo().getDisponibilita())
-			accessDeniedMVC(model);
+		if (!loggedIn.getRuolo().equals(Role.RUOLO_UTENTE))
+			return accessDeniedMVC(model);
 		
-		try {
-			this.srv.addPrenotazione(p);
-			model.addAttribute("prenotazione", p);
-			return "utente.html";
-		} catch (Exception e) {
-			// TODO ERROR
-			e.printStackTrace();
+		
+		Veicolo v= vSrv.getVeicoloById(p.getvId());
+		
+		if(v==null || !v.getDisponibilita())
+				return genericErrorMVC(model, "Veicolo non disponibile");
+		
+			Prenotazione pr= new Prenotazione();
+			pr.setInizio(p.getInizio());
+			pr.setFine(p.getFine());
+			pr.setVeicolo(v);
+			pr.setUtente(loggedIn);
+			pr.setStato(State.Prenotato);
 			
-		}
-		return "redirect:/";
+			this.srv.addPrenotazione(pr);
+			model.addAttribute("prenotazione", p);
+			return "utente";
+
 	}
 }
